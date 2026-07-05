d90c46275e59343a94cba567f5fbc2682177c50c
Manthan0501 <manthantiwari2697@gmail.com>
2026-07-05 14:28:42 +0530

    add

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
-    region       = "us-east-1"
+    bucket       = "manthan-tetris-001"
+    region       = "eu-west-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
     encrypt      = true
