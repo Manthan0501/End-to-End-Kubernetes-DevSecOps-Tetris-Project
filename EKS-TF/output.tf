719c30ea2df11c0484c18b160b1b1b33b9cc935b
Manthan Tiwari <manthantiwari2697@gmail.com>
2026-07-09 23:07:24 +0530

    Update output.tf

--- EKS-TF/output.tf
+++ EKS-TF/output.tf
@@ -10,13 +10,3 @@ output "aws_cluster_name" {
 output "aws_cluster_arn" {
   value = aws_eks_cluster.eks-cluster.arn
 }
-
-output "eks_node_private_ips" {
-  description = "Private IP addresses of EKS worker nodes"
-  value       = data.aws_instances.eks_nodes.private_ips
-}
-
-output "eks_node_public_ips" {
-  description = "Public IP addresses of EKS worker nodes"
-  value       = data.aws_instances.eks_nodes.public_ips
-}
